322f94c150508b48d5fe64f4d2457509bd65ba7c
Stu-me <stusundram@gmail.com>
2026-01-25 16:46:11 +0530

    basic work made -- x and o are convetible and winner declared

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,85 @@
 import { useState } from "react";
 import "./square.css";
 
-function Square() { // we declared our own tag(power of xml )
+function Square({ value , onSquareClick }) { // we declared our own tag(power of xml ) now we will use in the program
   
-  const [value,setValue] = useState(null); // we declare here state , function to change state and default value
- 
-  function handleClick(){
-    setValue('X');
-  }
+
   return (
     <button 
       className="square"
-      onClick={handleClick}
+      onClick={onSquareClick}
       >{value}</button>
   );
 }
 
-function Board() {
-  const [squares,setSquares] = useState(Array(9).fill(null));
+function Board() { // to rebuild the squares
+  const [xIsNext ,setXIsNext] = useState(true); // this is for changing between X and O
+  const [squares,setSquares] = useState(Array(9).fill(null)); // squares in array with 9 indexes
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner: " + winner;
+  }
+  else{
+    status = "Next player:" +(xIsNext? "X":"0");
+  }
+  function handleClick(i){                // reRendering 
+
+    if(squares[i] || calculateWinner(squares)){
+      return;  // if the square is not null or say have any value dont perform any operation 
+    }
+    const nextSquares = squares.slice();  // copy the whole squares array 
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }
+    else{
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);             // setSquares -- is the function to update the squares with nextSquares values
+    setXIsNext(!xIsNext);
+  }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
       </div>
 
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)} ></Square>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)} ></Square>
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)} ></Square>
       </div>
 
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)} ></Square>
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)} ></Square>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)} ></Square>
       </div>
     </>
   );
 }
-
+  function calculateWinner(squares){
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for(let i=0;i<lines.length;i++){
+      const [a,b,c] = lines[i];
+      if(squares[a] && squares[a] == squares[b] && squares[a] === squares[c]){
+        return squares[a]; // returning x or 0 ofc we will have to send one only 
+      }
+    }
+    return null 
+  }
 export default Board;
