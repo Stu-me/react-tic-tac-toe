6fbaea444d124b03ceb5a61da129cf006021dcab
Stu-me <stusundram@gmail.com>
2026-01-26 12:55:28 +0530

    moving between moves done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./square.css";
 
 function Square({ value , onSquareClick }) { // we declared our own tag(power of xml ) now we will use in the program
-  
-
   return (
     <button 
       className="square"
@@ -12,9 +10,7 @@ function Square({ value , onSquareClick }) { // we declared our own tag(power of
   );
 }
 
-function Board() { // to rebuild the squares
-  const [xIsNext ,setXIsNext] = useState(true); // this is for changing between X and O
-  const [squares,setSquares] = useState(Array(9).fill(null)); // squares in array with 9 indexes
+function Board({xIsNext , squares , onPlay}) { 
 
   const winner = calculateWinner(squares);
   let status;
@@ -24,21 +20,20 @@ function Board() { // to rebuild the squares
   else{
     status = "Next player:" +(xIsNext? "X":"0");
   }
-  function handleClick(i){                // reRendering 
-
-    if(squares[i] || calculateWinner(squares)){
-      return;  // if the square is not null or say have any value dont perform any operation 
-    }
-    const nextSquares = squares.slice();  // copy the whole squares array 
-    if(xIsNext){
-      nextSquares[i] = "X";
-    }
-    else{
-      nextSquares[i] = "O";
-    }
-    setSquares(nextSquares);             // setSquares -- is the function to update the squares with nextSquares values
-    setXIsNext(!xIsNext);
+function handleClick(i){                // reRendering 
+  
+  if(squares[i] || calculateWinner(squares)){
+    return;  // if the square is not null or say have any value dont perform any operation 
   }
+  const nextSquares = squares.slice();  // copy the whole squares array
+  if(xIsNext){
+    nextSquares[i] = "X";
+  }
+  else{
+    nextSquares[i] = "O";
+  }
+  onPlay(nextSquares);
+}
 
   return (
     <>
@@ -63,42 +58,67 @@ function Board() { // to rebuild the squares
     </>
   );
 }
-  function calculateWinner(squares){
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6],
-    ];
-    for(let i=0;i<lines.length;i++){
-      const [a,b,c] = lines[i];
-      if(squares[a] && squares[a] == squares[b] && squares[a] === squares[c]){
-        return squares[a]; // returning x or 0 ofc we will have to send one only 
-      }
+
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for(let i=0;i<lines.length;i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] == squares[b] && squares[a] === squares[c]){
+      return squares[a]; // returning x or 0 ofc we will have to send one only 
     }
-    return null 
   }
+  return null 
+}
 
   function Game(){ 
     // adding some state to track to Game component
-    const [xIsNext,setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]); // array of arrays of 9 nulls
-    const currentSquares = history[history.length-1]; // last array of arrays of history
+    const [currentMove,setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove]; // last array of arrays of history
+    const xIsNext = currentMove %2 === 0;
 
-    function handlePlay(nextSquares){
-      //Todo
-    }
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1),nextSquares]; // all the arrays along with nextSquares
+    setHistory(nextHistory); 
+    setCurrentMove(nextHistory.length-1); 
+    //Todo
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+    const moves = history.map((squares,move)=>{
+      let description;
+      if(move>0){
+        description = 'Go to move #' + move;
+      }
+      else{
+        description = 'Go to game start';
+      }
+      return(
+        <li key={move}>
+          <button onClick={()=> jumpTo(move)}>{description}</button>
+        </li>
+      );
+    });
+
     return( 
       <div className="game"> 
         <div className="game-board"> 
           <Board xIsNext = {xIsNext} squares= {currentSquares} onPlay = {handlePlay} />   {/* now we have complete board so now Games is parent component  */}
         </div>
         <div className="game-info">
-          <ol> {/* todo */} </ol>
+          <ol> {moves} </ol>
         </div>
 
       </div>
